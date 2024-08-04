94bdee6435e594c9c87991e475738291b0e30012
ctrldaviddee <princedavealex.20@gmail.com>
2024-08-04 19:30:18 +0100

    done with calculator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+// use std::{cell::RefCell, rc::Rc};
+use core::cell::RefCell;
+use std::{rc::Rc, result};
+
 slint::slint! {
     import { VerticalBox } from "std-widgets.slint";
 
@@ -9,8 +13,9 @@ slint::slint! {
         in property <string> text;
         min-height: 30px;
         min-width: 30px;
+        in property <brush> background: teal;
         Rectangle {
-            background: ta.pressed ? #747e88 : ta.has-hover ? #3cbf7b : teal;
+            background: ta.pressed ? #747e88 : ta.has-hover ? background.brighter(10%) : background;
             animate background { duration: 150ms; }
             border-radius: 4px;
             border-width: 2px;
@@ -26,7 +31,7 @@ slint::slint! {
     
     export component App inherits Window{
 
-        in property <int> value: 1;
+        in property <int> value: 0;
     
         GridLayout {
             padding: 10px;
@@ -39,39 +44,87 @@ slint::slint! {
                 Button{text: "1";}
                 Button{text: "2";}
                 Button{text: "3";}
+                Button{text: "+"; background: gray;}
             }
             Row{
                 Button{text: "4";}
                 Button{text: "5";}
                 Button{text: "6";}
+                Button{text: "-"; background: gray;}
             }
             Row{
                 Button{text: "7";}
                 Button{text: "8";}
                 Button{text: "9";}
+                Button{text: "*"; background: gray;}
             }
             Row{
                 Button{text: "0";}
+                Button{
+                    text: "=";
+                    col: 2;
+                    background: @linear-gradient(-20deg, #b38fae, #a365e3);
+                }
+                Button{text: "/"; background: gray;}
             }
         }
     }
 }
 
+#[derive(Default)]
+struct CalcState{
+    prev_value: i32,
+    current_value: i32,
+    operator: slint::SharedString
+}
+
 fn main() {
+    // https://blog.ashutoshkrris.in/how-to-download-youtube-playlist-using-python
+//tracing = "0.1"
+//libloading = "0.7"
     let app = App::new().unwrap();
     let weak = app.as_weak();
 
+    let state = Rc::new(RefCell::new(CalcState::default()));
+
     app.global::<CalcLogic>().on_button_pressed(move | value | {
+
         let app = weak.unwrap();
-        let val: i32 = value.parse().unwrap();
-        app.set_value(app.get_value() * 10 + val);
-    },
+
+        let mut state = state.borrow_mut();
+
+        if let Ok(val) = value.parse::<i32>() {
+            state.current_value *= 10;
+            state.current_value += val;
+            app.set_value(state.current_value);
+            return;
+        }
+
+        if value.as_str() == "=" {
+            let result = match state.operator.as_str() {
+                "+" => state.prev_value + state.current_value,
+                "-" => state.prev_value - state.current_value,
+                "*" => state.prev_value * state.current_value,
+                "/" => state.prev_value / state.current_value,
+                _ => return
+            };
+            
+            app.set_value(result);
+
+            state.current_value = 0;
+
+            state.prev_value = result;
+
+            state.operator = Default::default();
+
+        }else{
+            state.operator = value.clone();
+            state.prev_value = state.current_value;
+            state.current_value = 0;
+        }
+    }
 );
     
     app.run().unwrap();
     println!("Hello, world!");
 }
-
-// https://blog.ashutoshkrris.in/how-to-download-youtube-playlist-using-python
-//tracing = "0.1"
-//libloading = "0.7"
