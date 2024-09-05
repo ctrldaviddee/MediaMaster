7724643f6a57c8c37d466cb14a957fe75a8d5998
ctrldaviddee <princedavealex.20@gmail.com>
2024-09-06 00:23:27 +0100

    started page

--- build.rs
+++ build.rs
@@ -1,3 +1,8 @@
+const MAIN_WINDOW: &str = "ui/welcome.slint";
+
 fn main(){
-    slint_build::compile("ui/welcome.slint").unwrap();
+    slint_build::compile_with_config(
+        MAIN_WINDOW,
+        slint_build::CompilerConfiguration::new().with_library_paths(vivi_ui::import_paths()),
+    ).unwrap();
 }
